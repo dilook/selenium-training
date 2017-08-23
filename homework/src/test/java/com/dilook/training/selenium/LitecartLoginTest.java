84c1e2a2a9007ba70758154115f732f8f891316c
dilook <bss4work@yandex.ru>
2017-08-24 00:46:44 +0400

    add FF_ESR & FF_NIGHTLY + refactor

--- homework/src/test/java/com/dilook/training/selenium/LitecartLoginTest.java
+++ homework/src/test/java/com/dilook/training/selenium/LitecartLoginTest.java
@@ -4,11 +4,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.IOException;
+
+import static com.dilook.training.selenium.WebDriverTestUtils.selectWebDriver;
 
 /**
  * Created by Di on 16.08.2017.
@@ -18,28 +20,9 @@ public class LitecartLoginTest {
     private WebDriver webDriver;
 
     @Before
-    public void start() {
+    public void start() throws IOException {
         DesiredCapabilities caps = new DesiredCapabilities();
         webDriver = selectWebDriver(caps);
-    }
-
-    private WebDriver selectWebDriver(DesiredCapabilities caps) {
-        String browser = System.getProperty("browser", "CR");
-        System.out.println(browser);
-
-        switch (browser) {
-            case "FF":
-                return caps == null ? new FirefoxDriver() : new FirefoxDriver(caps);
-
-            case "IE":
-                return caps == null ? new InternetExplorerDriver() : new InternetExplorerDriver(caps);
-
-            case "CR":
-                return caps == null ? new ChromeDriver() : new ChromeDriver(caps);
-
-            default:
-                return caps == null ? new ChromeDriver() : new ChromeDriver(caps);
-        }
     }
 
     @Test
@@ -48,6 +31,8 @@ public class LitecartLoginTest {
         webDriver.findElement(By.name("username")).sendKeys("admin");
         webDriver.findElement(By.name("password")).sendKeys("admin");
         webDriver.findElement(By.name("login")).click();
+
+        System.out.println(((HasCapabilities) webDriver).getCapabilities());
     }
 
     @After
---
+++ homework/src/test/java/com/dilook/training/selenium/WebDriverTestUtils.java
@@ -0,0 +1,65 @@
+package com.dilook.training.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Di on 24.08.2017.
+ */
+public class WebDriverTestUtils {
+
+    protected static WebDriver selectWebDriver(DesiredCapabilities caps) throws IOException {
+        String browser = System.getProperty("browser", "CR");
+        System.out.println(browser);
+
+        switch (browser) {
+            case "FF":
+                return caps == null ? new FirefoxDriver() : new FirefoxDriver(caps);
+
+            case "IE":
+                return caps == null ? new InternetExplorerDriver() : new InternetExplorerDriver(caps);
+
+            case "CR":
+                return caps == null ? new ChromeDriver() : new ChromeDriver(caps);
+
+            case "FFESR":
+                FirefoxOptions fo = new FirefoxOptions()
+                        .setLegacy(true)
+                        .setBinary(extractESRPath("src/test/resources/caps.properties", "esr.path"));
+                return new FirefoxDriver(fo);
+
+            case "FFNIGHT":
+                fo = new FirefoxOptions()
+                        .setBinary(extractESRPath("src/test/resources/caps.properties", "nightly.path"))
+                        .setProfile(new FirefoxProfile());
+
+                return new FirefoxDriver(fo);
+
+            default:
+                return caps == null ? new ChromeDriver() : new ChromeDriver(caps);
+        }
+    }
+
+    private static String extractESRPath(String propFilePath, String propName) throws IOException {
+        Properties properties = new Properties();
+
+        try (FileReader fr = new FileReader(new File(propFilePath))) {
+            properties.load(fr);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        }
+
+        return properties.getProperty(propName);
+    }
+}
