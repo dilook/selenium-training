19c4bc581d5f3c29e77981baf73045b3f9d36a90
dilook <bss4work@yandex.ru>
2017-09-05 00:10:26 +0400

    add wait condition

--- homework/src/test/java/com/dilook/training/selenium/AdminLoginTest.java
+++ homework/src/test/java/com/dilook/training/selenium/AdminLoginTest.java
@@ -3,6 +3,8 @@ package com.dilook.training.selenium;
 import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
 /**
  * Created by Di on 29.08.2017.
  */
@@ -11,6 +13,7 @@ public class AdminLoginTest extends BaseTest {
     @BeforeClass
     public static void openAdminSideTest() {
         webDriver.navigate().to("https://litecart.000webhostapp.com/admin/login.php");
+        webDriverWait.until(titleIs("My Store"));
         webDriver.findElement(By.name("username")).sendKeys("admin");
         webDriver.findElement(By.name("password")).sendKeys("admin");
         webDriver.findElement(By.name("login")).click();
