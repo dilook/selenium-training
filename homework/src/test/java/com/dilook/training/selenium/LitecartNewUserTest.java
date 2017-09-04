dcbd9390d18aaaa7e6f0f4ad078c86bedf34cb01
dilook <bss4work@yandex.ru>
2017-09-04 12:14:20 +0300

    hw#11 done

--- homework/src/test/java/com/dilook/training/selenium/LitecartNewUserTest.java
+++ homework/src/test/java/com/dilook/training/selenium/LitecartNewUserTest.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 /**
  * Created by Di on 03.09.2017.
  */
 public class LitecartNewUserTest extends OpenClientPageTest {
-    
+
     @Test
     public void registerUser() {
         navigateTo("https://litecart.000webhostapp.com/en/create_account");
@@ -21,21 +23,32 @@ public class LitecartNewUserTest extends OpenClientPageTest {
         webDriver.findElement(By.name("address2")).sendKeys("Mohave, Value 8");
         webDriver.findElement(By.name("postcode")).sendKeys("15623");
         webDriver.findElement(By.name("city")).sendKeys("City 17");
+
         Select country = new Select(webDriver.findElement(By.name("country_code")));
         country.selectByVisibleText("United States");
-        //webDriverWait.until(visibilityOf(webDriver.findElement(By.cssSelector("select[name = 'zone_code']"))));
+
+        webDriverWait.until(elementToBeClickable(webDriver.findElement(By.cssSelector("select[name = 'zone_code']"))));
         Select zone = new Select(webDriver.findElement(By.cssSelector("select[name = 'zone_code']")));
         zone.selectByVisibleText("Guam");
-        String rand = generator(3);
-        webDriver.findElement(By.name("email")).sendKeys("ex" + rand + "@test.com");
+
+        String email = "ex" + generator(3) + "@test.com";
+        System.out.println(email);
+        webDriver.findElement(By.name("email")).sendKeys(email);
+
         webDriver.findElement(By.name("phone")).sendKeys("12345678");
         webDriver.findElement(By.name("password")).sendKeys("passwd");
         webDriver.findElement(By.name("confirmed_password")).sendKeys("passwd");
         webDriver.findElement(By.name("create_account")).click();
 
-        webDriverWait.until(titleIs("Online Store | My Store"));
+        webDriverWait.until(visibilityOf(webDriver.findElement(By.id("box-account")).findElement(By.linkText("Logout"))));
 
         webDriver.findElement(By.id("box-account")).findElement(By.linkText("Logout")).click();
 
+        webDriver.findElement(By.name("email")).sendKeys(email);
+        webDriver.findElement(By.name("password")).sendKeys("passwd");
+        webDriver.findElement(By.name("login")).click();
+
+        webDriverWait.until(visibilityOf(webDriver.findElement(By.id("box-account")).findElement(By.linkText("Logout"))));
+        webDriver.findElement(By.id("box-account")).findElement(By.linkText("Logout")).click();
     }
 }
